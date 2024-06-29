c5c20e828df5af495f792c690edf7ff3c5c4f6cb
Danielito games <scalleyovera@gmail.com>
2024-06-28 21:35:31 -0600

    creacion de proyecto spring

---
+++ src/main/java/com/example/LP2_Proyecto/Lp2ProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LP2_Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2ProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LP2_Proyecto/Lp2ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LP2_Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
